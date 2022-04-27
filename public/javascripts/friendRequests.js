bd1555db06201c8132c18eb34a46dcd8d1d937eb
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-04-27 12:10:38 +0100

    request sent button disabled

--- public/javascripts/friendRequests.js
+++ public/javascripts/friendRequests.js
@@ -7,13 +7,15 @@ document.addEventListener('DOMContentLoaded', () => {
   for (i = 0; i < addFriendButtons.length; i++) {
     console.log('Found friend button')
     addFriendButtons[i].addEventListener('click', (event) => {
-      const parent = event.target.parentElement
-      const postId = parent.getAttribute('id')
-      const authorId = parent.children[7].getAttribute('id')
-      event.target.innerText = "Request Sent"
-      
+      if (event.target.getAttribute('class') == 'add_friend_button') {
+        const parent = event.target.parentElement
+        const postId = parent.getAttribute('id')
+        const authorId = parent.children[7].getAttribute('id')
+        event.target.innerText = "Request Sent"
+        event.target.setAttribute('class', 'request_badge')
 
-      socket.emit('addFriend', { postId: postId, requestUser: userId })
+        socket.emit('addFriend', { postId: postId, requestUser: userId })
+      }
     })
   }
 
