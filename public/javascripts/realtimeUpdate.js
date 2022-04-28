494ab1c0ce8e826791bd04322a90941cd99cdce9
Daniel Taplin <98479830+dantaplin992@users.noreply.github.com>
2022-04-28 16:38:18 +0100

    everything except comments

--- public/javascripts/realtimeUpdate.js
+++ public/javascripts/realtimeUpdate.js
@@ -42,9 +42,67 @@ document.addEventListener('DOMContentLoaded', () => {
     newChild.setAttribute('class', 'post')
     newChild.setAttribute('id', newPostId)
 
+    const postHeader = document.createElement('div')
+    postHeader.setAttribute('class', 'post-header')
+
     const authorName = document.createElement('div')
     authorName.setAttribute('class', 'post_author')
     authorName.innerText = `${newPost.authorFirstName} ${newPost.authorSurname}`
+
+    const authorOptions = document.createElement('div')
+    authorOptions.setAttribute('class', 'author-options')
+
+    const deleteRow = document.createElement('div')
+    deleteRow.setAttribute('class', 'delete_button_container')
+    const deleteForm = document.createElement('form')
+    deleteForm.setAttribute('action', `/posts/delete/${newPostId}`)
+    deleteForm.setAttribute('method', 'post')
+    const hiddenFormInput = document.createElement('input')
+    hiddenFormInput.setAttribute('type', 'hidden')
+    hiddenFormInput.setAttribute('name', 'userId')
+    hiddenFormInput.setAttribute('value', userId)
+    const deleteButtonSpan = document.createElement('span')
+    deleteButtonSpan.setAttribute('id', 'delete_post')
+    const deleteButton = document.createElement('button')
+    deleteButton.setAttribute('id', 'delete_post')
+    deleteButton.setAttribute('class', 'submit-button')
+    deleteButton.innerText = "Delete post"
+    deleteButtonSpan.appendChild(deleteButton)
+    nestItems(deleteForm, [hiddenFormInput, deleteButtonSpan])
+    deleteRow.appendChild(deleteForm)
+
+    const editRow = document.createElement('div')
+    editRow.setAttribute('class', 'delete_button_container')
+    const editForm = document.createElement('form')
+    editForm.setAttribute('action', `/posts/edit/${newPostId}`)
+    editForm.setAttribute('method', 'get')
+    const hiddenPostMsg = document.createElement('input')
+    hiddenPostMsg.setAttribute('type', 'hidden')
+    hiddenPostMsg.setAttribute('name', 'post_msg')
+    hiddenPostMsg.setAttribute('value', newPost.message)
+    const hiddenUserId = document.createElement('input')
+    hiddenUserId.setAttribute('type', 'hidden')
+    hiddenUserId.setAttribute('name', 'userId')
+    hiddenUserId.setAttribute('value', userId)
+    const hiddenPostId = document.createElement('input')
+    hiddenPostId.setAttribute('type', 'hidden')
+    hiddenPostId.setAttribute('name', 'post_id')
+    hiddenPostId.setAttribute('value', newPostId)
+    const editSpan = document.createElement('span')
+    editSpan.setAttribute('id', 'edit_post')
+    const editPostButton = document.createElement('button')
+    editPostButton.setAttribute('class', 'submit-button')
+    editPostButton.innerText = "Edit Post"
+    editSpan.appendChild(editPostButton)
+    nestItems(editForm, [hiddenPostMsg, hiddenUserId, hiddenPostId, editSpan])
+    editRow.appendChild(editForm)
+
+    const space = document.createElement('span')
+    space.innerText = ' '
+
+    nestItems(authorOptions, [editRow, space, deleteRow])
+
+    nestItems(postHeader, [authorName, authorOptions])
 
     const postMessage = document.createElement('div')
     postMessage.setAttribute('class', 'post_content')
@@ -72,23 +130,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     nestItems(likesRow, [likesId, likesCount, likeButton])
 
-    const deleteRow = document.createElement('div')
-    deleteRow.setAttribute('class', 'delete_button_container')
-    const deleteForm = document.createElement('form')
-    deleteForm.setAttribute('action', `/posts/delete/${newPostId}`)
-    deleteForm.setAttribute('method', 'post')
-    const hiddenFormInput = document.createElement('input')
-    hiddenFormInput.setAttribute('type', 'hidden')
-    hiddenFormInput.setAttribute('name', 'userId')
-    hiddenFormInput.setAttribute('value', userId)
-    const deleteButton = document.createElement('button')
-    deleteButton.setAttribute('id', 'delete_post')
-    deleteButton.innerText = "Delete post"
-    deleteForm.appendChild(hiddenFormInput)
-    deleteForm.appendChild(deleteButton)
-    deleteRow.appendChild(deleteForm)
-
-    nestItems(newChild, [authorName, postMessage, postDate, likesRow, deleteRow])
+    nestItems(newChild, [postHeader, postMessage, postDate, likesRow])
 
     postsFeed.insertBefore(newChild, postsFeed.firstChild)
   })
